6cf9e303e6528f644f6ce763e38f13337934da56
jislane.santos <jislane.santos@ambevtech.com.br>
2021-10-20 15:37:35 -0300

    Cards com imagens dos pokemons

--- pokebev/src/components/Pokemon/Pokemon.tsx
+++ pokebev/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import  {useEffect, useState} from 'react'
 import * as styles from './Pokemon.module.css';
 
 type PokemonData = {
@@ -7,10 +7,35 @@ type PokemonData = {
   url: string
 }
 
-const Pokemon = (parametros: PokemonData) => (
-  <div className={styles.default.Pokemon} data-testid="Pokemon">
-    {parametros.name}
-  </div>
-);
+function Pokemon  (parametros: PokemonData)  {
+
+   const [pokemon, setPokemon] = useState<any>(undefined);
+
+  useEffect(() => { //faz a chamada, mas nao tem resposta
+    fetch(parametros.url)
+    .then((response) => response.json()) //espera a resposta
+    .then((data) => setPokemon(data)); //espera o json ficar pronto
+  }, []);
+  
+ if(!pokemon){
+   //Resposta enquanto não temos a informação
+  return (
+  
+    <div className={styles.default.Pokemon} data-testid="Pokemon">
+      {parametros.name}
+    </div>
+  )
+  } else{ console.log(pokemon)
+    //Resposta quando temos a informação
+  return (
+  
+    <div className={styles.default.Pokemon} data-testid="Pokemon">
+      <img src={pokemon.sprites.front_default}></img>
+      <img src={pokemon.sprites.back_default}></img>
+      {parametros.name}
+    </div>
+  ) 
+  }
+}
 
 export default Pokemon;
