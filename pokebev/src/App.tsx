159e34539c7ebfc90cc922363b2ba4e08d3a4488
Giu Zambot <giuzambot@gmail.com>
2021-10-21 16:21:03 -0300

    Listagem de gerações de seus pokemons

--- pokebev/src/App.tsx
+++ pokebev/src/App.tsx
@@ -1,30 +1,16 @@
-import  {useEffect, useState} from 'react'
 import './App.css';
-import Pokemon from './components/Pokemon/Pokemon';
+import Games from './components/Games/Games';
+
 
 type PokemonData = {
- 
   name: string,
   url: string
 }
 
 function AppPokemon() {
-
-  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
-
-  useEffect(() => { //faz a chamada, mas nao tem resposta
-    fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
-    .then((response) => response.json()) //espera a resposta
-    .then((data) => setPokemons(data.results)); //espera o json ficar pronto
-  }, []);
-
-
-
   return (
-    <div className="Pokemon-List">
-      {
-        pokemons.length > 0 && pokemons.map((pokemon) => <Pokemon url={pokemon.url} name={pokemon.name}></Pokemon>) //se a primeira for falsa, para.
-      }
+    <div>
+      <Games />
     </div>
   )
 }
