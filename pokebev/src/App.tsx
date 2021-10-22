779fd03d74211e744711b3ff020341eaa57521f7
IzabelaGonzaga <izabela.silva@ambevtech.com.br>
2021-10-22 08:18:48 -0300

    alterações grupo 4

--- pokebev/src/App.tsx
+++ pokebev/src/App.tsx
@@ -1,9 +1,10 @@
 import  {useEffect, useState} from 'react'
 import './App.css';
 import Pokemon from './components/Pokemon/Pokemon';
+import ModalScreen from './Modal';
+import { Button } from 'react-bootstrap';
 
 type PokemonData = {
- 
   name: string,
   url: string
 }
@@ -18,16 +19,23 @@ function AppPokemon() {
     .then((data) => setPokemons(data.results)); //espera o json ficar pronto
   }, []);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
 
   return (
-    <div className="Pokemon-List">
-      {
-        pokemons.length > 0 && pokemons.map((pokemon) => <Pokemon url={pokemon.url} name={pokemon.name}></Pokemon>) //se a primeira for falsa, para.
-      }
-    </div>
-  )
+    <>
+      <div className='LocationsButton'>
+        <Button className='mt-4' variant='secondary' onClick={() => setIsModalVisible(true)}> Locations </Button>
+        {isModalVisible? <ModalScreen  /> : null} 
+      </div>
+      <div className="Pokemon-List">
+        {
+          pokemons.length > 0 && pokemons.map((pokemon) => <Pokemon url={pokemon.url} name={pokemon.name}></Pokemon>) //se a primeira for falsa, para.
+        }
+      </div>
+      
+    </>
+  );
 }
 
 export default AppPokemon;
-
