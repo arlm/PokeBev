1721d1dab54204edf1bfea810fbd1d5456d95f0d
luana.kuster <luana.kuster@ambevtech.com.br>
2021-10-25 13:58:09 -0300

    Resolução inicial - Location

--- pokebev/src/components/Location/Location.tsx
+++ pokebev/src/components/Location/Location.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import styles from './Location.module.css';
+import { useState, useEffect } from "react";
 
-function Location(){
-  return(
+interface LocationType {
+  name: any,
+  region: any,
+  areas: any
+}
+
+export default function Location(){
+  const [id, setId] = useState<any>(0)
+  const [locations, setLocations] = useState<any>(undefined)
+  
+  
+  function obterLocation () {
+    
+    
+      fetch(`https://pokeapi.co/api/v2/location/${id}`)
+      .then((resp) => resp.json())
+      .then((dados) => setLocations(dados));   
+    
+  };
+  
+  return (
+    <>
     <div>
+      
+      <input type="number" value={id} onChange={e => setId(e.currentTarget.value)}/>
+      <button onClick={obterLocation}>Obter Location</button>
+
     </div>
+    
+    <ul>
+      <li>Name: {locations?.name ?? "Selecione um id"}</li>
+      <li>Region: {locations?.region.name ?? "Selecione um id"}</li>
+      <li>Areas: {locations?.areas[0].name ?? "Selecione um id"}</li>
+    </ul>
+    
+    </>
+  
   );
 }
 
-export default Location;
+
--- pokebev/src/components/Location/ModalLocations.tsx
+++ pokebev/src/components/Location/ModalLocations.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button, Modal, ModalTitle, ModalBody } from "react-bootstrap";
 import ModalHeader from "react-bootstrap/esm/ModalHeader";
-
+import Location from "./Location";
 
 export default function ScreenLocations() {
     const [show, setShow] = useState(false);
@@ -17,7 +17,7 @@ export default function ScreenLocations() {
           </ModalHeader>
           <ModalBody>
             <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut aliquam fuga officiis maiores excepturi quas consequatur nihil officia rerum esse voluptatem ratione voluptates veniam cupiditate iusto nemo provident, quidem similique!
+                <Location/>
             </p>
           </ModalBody>
       </Modal>
