0b3184bf1f761e9306aa790fbfdc13cd78429c9c
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-10-25 15:45:38 -0300

    arrumando identação do código

--- pokebev/src/components/Evolution/Evolution.tsx
+++ pokebev/src/components/Evolution/Evolution.tsx
@@ -22,7 +22,6 @@ function Evolution (params: PokemonDataEvolution) {
     return ( <div>Aguarde</div> );
   } else {
 
-
     const metaBaseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/' 
     const pokeBaseUrl = evolucao.chain.species.url                                                       
     const metaEvolutionUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
@@ -30,92 +29,56 @@ function Evolution (params: PokemonDataEvolution) {
 
     let evolutionBaseId = pokeBaseUrl.substring(pokeBaseUrl.indexOf("cies") + 5).split("/")[0]
 
-   
-
-   
-
     const srcBaseImg = metaBaseUrl + evolutionBaseId + ".svg"
-
-    
 
     let evolution2 = []
     if(!evolucao.chain.evolves_to[0].evolves_to[0]){
       evolution2.push("não há mais evoluções")
       
-    }else{evolution2.push(evolucao.chain.evolves_to[0].evolves_to[0].species.name)}
-
-  
-
-    
-    
+    } else {
+      evolution2.push(evolucao.chain.evolves_to[0].evolves_to[0].species.name)
+    }
 
     const pokeEvolutionUrl = urlAlt.substring(urlAlt.indexOf("cies") + 5).split("/")[0] + ".png"
     const srcEvolutionImg = metaEvolutionUrl + pokeEvolutionUrl 
     console.log(srcEvolutionImg)
-
-        
-
-    
    
-if(!evolucao.chain.evolves_to[0].evolves_to[0]){
-  return (
+    if(!evolucao.chain.evolves_to[0].evolves_to[0]){
+      return (
     
-  
-    <div className={styles.Evolution} data-testid="Evolution">         
-      
+        <div className={styles.Evolution} data-testid="Evolution">         
           
-     
-      <img className={styles.PokemonBase} alt= {evolucao.chain.species.name} src= {srcBaseImg} /> 
-      <h3>{evolucao.chain.species.name}</h3>    
-      <img className={styles.PokemonMini} alt= {evolucao.chain.evolves_to[0].species.name} src= {srcEvolutionImg} />
-      <p>{evolucao.chain.evolves_to[0].species.name}</p>
-           
-      <p>{evolution2}</p>
-       
-   
-    </div>    
-      
- 
-
-  ) }else{
-
-    var urlAlt2 = evolucao.chain.evolves_to[0].evolves_to[0].species.url
-
-    let lastEvolution = urlAlt2.substring(urlAlt2.indexOf("cies") + 5).split("/")[0]
-
-   
-
-    const pokeEvolutionUrl2 = lastEvolution + ".png"
-    const srcEvolutionImg2 = metaEvolutionUrl + pokeEvolutionUrl2
-    
-    
-    
-    
-    
-    return (
-    
-  
-    <div className={styles.Evolution} data-testid="Evolution">         
-      
+          <img className={styles.PokemonBase} alt= {evolucao.chain.species.name} src= {srcBaseImg} /> 
+            <h3>{evolucao.chain.species.name}</h3>    
+          <img className={styles.PokemonMini} alt= {evolucao.chain.evolves_to[0].species.name} src= {srcEvolutionImg} />
+            <p>{evolucao.chain.evolves_to[0].species.name}</p>  
+            <p>{evolution2}</p>
           
-     
-      <img className={styles.PokemonBase} alt= {evolucao.chain.species.name} src= {srcBaseImg} /> 
-      <h3>{evolucao.chain.species.name}</h3>    
-      <img className={styles.PokemonMini}  alt= {evolucao.chain.evolves_to[0].species.name} src= {srcEvolutionImg} />
-      <p>{evolucao.chain.evolves_to[0].species.name}</p>
-      <img className={styles.PokemonMini} alt= {evolucao.chain.evolves_to[0].evolves_to[0].species.name} src= {srcEvolutionImg2} />      
-      <p>{evolution2}</p>
-       
-   
-    </div> )}
-  
+      
+        </div>    
+
+      )} else {
+
+        var urlAlt2 = evolucao.chain.evolves_to[0].evolves_to[0].species.url
+        let lastEvolution = urlAlt2.substring(urlAlt2.indexOf("cies") + 5).split("/")[0]
+
+        const pokeEvolutionUrl2 = lastEvolution + ".png"
+        const srcEvolutionImg2 = metaEvolutionUrl + pokeEvolutionUrl2
+          
+        return (
+          
+          <div className={styles.Evolution} data-testid="Evolution">         
+            
+            <img className={styles.PokemonBase} alt= {evolucao.chain.species.name} src= {srcBaseImg} /> 
+              <h3>{evolucao.chain.species.name}</h3>    
+            <img className={styles.PokemonMini}  alt= {evolucao.chain.evolves_to[0].species.name} src= {srcEvolutionImg} />
+              <p>{evolucao.chain.evolves_to[0].species.name}</p>
+            <img className={styles.PokemonMini} alt= {evolucao.chain.evolves_to[0].evolves_to[0].species.name} src= {srcEvolutionImg2} />      
+              <p>{evolution2}</p>
+              
+          </div> 
+          )}
+    }
 }
-}
-// const Evolution = (params: PokemonDataEvolution) => (
-  
-//   <div className={styles.Evolution} data-testid="Evolution">
-//     Evolution Component
-//   </div>
-// );
 
 export default Evolution;
