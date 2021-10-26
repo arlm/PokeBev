f3189fe94e2d6552093ccc36de730d0d375372d8
rafaela.bittencourt@ambevtech.com.br <rafaela.bittencourt@ambevtech.com.br>
2021-10-26 15:37:12 -0300

    Lista de locations e tratamento de erros

--- pokebev/src/components/Location/Location.tsx
+++ pokebev/src/components/Location/Location.tsx
@@ -9,34 +9,46 @@ interface LocationType {
 }
 
 export default function Location(){
-  const [id, setId] = useState<any>(0)
+  const [name, setName] = useState<any>('')
   const [locations, setLocations] = useState<any>(undefined)
+  const [lista, setLista] = useState<any>([])
   
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/location')
+      .then((resp) => resp.json())
+      .then((dados) => setLista(dados.results))
+      .catch(() => alert('Erro!'));
+  }, []);
   
   function obterLocation () {
-    
-    
-      fetch(`https://pokeapi.co/api/v2/location/${id}`)
+    if (name || name.length > 0) {
+      fetch(`https://pokeapi.co/api/v2/location/${name}`)
       .then((resp) => resp.json())
-      .then((dados) => setLocations(dados));   
-    
+      .then((dados) => setLocations(dados))
+      .catch(() => alert('Erro!'));
+    }
+    else {
+      alert('Esse espaço não pode ficar vazio.');
+    }
   };
   
   return (
     <>
+    
     <div>
-      
-      <input type="number" value={id} onChange={e => setId(e.currentTarget.value)}/>
-      <button onClick={obterLocation}>Obter Location</button>
-
+      <input placeholder="Escolha uma location." list="pokemon" required minLength={1} value={name} onChange={e => setName(e.currentTarget.value)}/>
+      <datalist id="pokemon"> {Object.keys(lista).length > 0 && lista.map((s: any) => <option value={s.name} key={s.name} />)}
+      </datalist>
+      <button onClick={ obterLocation }>Obter Location</button>
     </div>
-    
+
+    { locations &&
     <ul>
-      <li>Name: {locations?.name ?? "Selecione um id"}</li>
-      <li>Region: {locations?.region.name ?? "Selecione um id"}</li>
-      <li>Areas: {locations?.areas[0].name ?? "Selecione um id"}</li>
+      <li>Nome: {locations?.name ?? "Selecione um id"}</li>
+      <li>Região: {locations?.region.name ?? "Selecione um id"}</li>
+      <li>Áreas: {locations?.areas[0].name ?? "Selecione um id"}</li>
     </ul>
-    
+    }
     </>
   
   );
