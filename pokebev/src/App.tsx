1b702fcfc9db12e282ec90c0fa4f0e7ce7966473
mflilian <mayhharamorais@gmail.com>
2021-10-26 16:25:47 -0300

    imports other components to app

--- pokebev/src/App.tsx
+++ pokebev/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Games from './components/Games/Generation/Games';
+import Pokedexes from './components/Games/Pokedexes/Pokedexes';
+import Version from './components/Games/Version/Version';
+import VersionGroup from './components/Games/VersionGroup/VersionGroup';
 
 function AppPokemon() {
   return (
     <div >
       <Games />
+      <Pokedexes />
     </div>
   )
 }
