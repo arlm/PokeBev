6cdd649fe709096a781db8848035d62401aa9b59
cyro.costa <cyro.costa@ambevtech.com.br>
2021-10-27 08:15:54 -0300

    adicionando funções verificadoras no EvolutionCard

--- pokebev/src/components/EvolutionCard/EvolutionCard.tsx
+++ pokebev/src/components/EvolutionCard/EvolutionCard.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
+import Evolution from '../Evolution/Evolution';
 import styles from './EvolutionCard.module.css';
 
-const EvolutionCard = (props:{showModal:boolean})=>{  
+const EvolutionCard = (props:{showModal:boolean, pokeName?:string, objetoEvolucao?:any})=>{
   const [fullscreen, setFullscreen] = useState<
     | true
     | "sm-down"
@@ -12,36 +13,50 @@ const EvolutionCard = (props:{showModal:boolean})=>{
     | "xxl-down"
     | undefined
   >(true);
+
   const [show, setShow] = useState(false);
 
   function handleShow() {
     setFullscreen(true);
     setShow(true);
-  } 
-  
+  }
+
   useEffect(() => {
-    if(props.showModal){
-    handleShow();
-  }else{
-    setShow(false)
-  };
+    if (props.showModal) {
+      handleShow();
+    } else {
+      setShow(false);
+    }
   }, []);
 
+  const levelBase = props.objetoEvolucao.chain.evolves_to[0].evolution_details[0].trigger
+      .name;
 
+  function showIf(){
+    if(levelBase){return levelBase}
+    
+  }
 
-
-  return(    
-    <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal</Modal.Title>
-        </Modal.Header>
-        <Modal.Body><div className={styles.EvolutionCard} data-testid="EvolutionCard">
-    EvolutionCard Component
-    </div></Modal.Body>
-      </Modal>
-  
-  );
    
+
+  return (
+    <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.pokeName}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className={styles.EvolutionCard} data-testid="EvolutionCard">
+          <p>
+            
+            
+            {
+              showIf()
+            }
+          </p>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 };
 
 export default EvolutionCard;
