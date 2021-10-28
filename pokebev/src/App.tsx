67fa63bf1f95817896f939ade28547f1453defc3
Úrsula <ursula.silva@ambevtech.com.br>
2021-10-28 13:32:29 -0300

    Barra de Navegação, Principal Page.

--- pokebev/src/App.tsx
+++ pokebev/src/App.tsx
@@ -4,6 +4,17 @@ import { BerryData } from './components/Models/interfaces';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Contests from './components/Contest/Contest';
 import './App.css';
+import Home from './components/Home/Home';
+import { Route, Switch, Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import Pokemon from './components/Pokemon/Pokemon';
+import Item from './components/Item/Item';
+import Contest from './components/Contest/Contest';
+import Game from './components/Game/Game';
+import Evolution from './components/Evolution/Evolution';
+import Location from './components/Location/Location';
+
+
 
 function AppPokemon() {
 
@@ -29,22 +40,57 @@ function AppPokemon() {
   // }, []);
 
   return (
-    
     <div>
-      <h1>Berry</h1>
-      <div className="Berries-List">
-        {
-          berries.length > 0 && berries.map((berry) => <Berry url={berry.url} name={berry.name}  ></Berry>) //se a primeira for falsa, para.
-        }
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Item">Item</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        
+      <Switch>
+        <Container >
+          <Route exact path="/">
+            <Home />
+          </ Route>
 
-      {/* <div className="Contests-List">
-        {
-          contests.length > 0 && pokemons.map((pokemon) => <Contests url={pokemon.url} name={pokemon.name} ></Contests>) //se a primeira for falsa, para.
-        }
-      </div> */}
+          {/* <Route exact path="/Pokemon">
+            <Pokemon />
+          </ Route> */}
+
+          <Route exact path="/Item">
+            <Item />
+          </ Route>
+
+          <Route exact path="/Contests">
+            <Contest />
+          </ Route>
+
+          <Route exact path="/Games">
+            <Game />
+          </ Route>
+
+          {/* <Route exact path="/Berry">
+            <Berry />
+          </ Route> */}
+
+          <Route exact path="/Evolution">
+            <Evolution />
+          </ Route>
+
+          <Route exact path="/Locations">
+            <Location />
+          </ Route>
+        </Container>
+      </Switch>
     </div>
-    
+        
   )
 }
 
