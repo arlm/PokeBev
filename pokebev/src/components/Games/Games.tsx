b914f56048445b971c73d9356f877a47d019f118
gabriela.berte <you@example.com>
2021-10-28 16:58:47 -0300

    Mudando o estilo da pagina

--- pokebev/src/components/Games/Games.tsx
+++ pokebev/src/components/Games/Games.tsx
@@ -2,14 +2,34 @@ import VersionGroup from "./VersionGroup/VersionGroup";
 import Version from "./Version/Version";
 import Pokedexes from "./Pokedexes/Pokedexes";
 import Generations from "./Generation/Generations";
+import styles from "./Games.module.css"
+import "./Games.css"
+import { Nav, Tabs, Tab } from "react-bootstrap";
 
 export default function Games() {
-    return (
-        <>
-            <Generations />
-            <Pokedexes />
-            <Version />
-            <VersionGroup />
-        </>
-    )
+  return (
+    <>
+
+      <Tabs
+        defaultActiveKey="Generations"
+        id="uncontrolled-tab-example"
+        className={styles.Games}
+      >
+        <Tab eventKey="Generations" title="Generations">
+        <Generations />
+        </Tab>
+        <Tab eventKey="Pokedexes" title="Pokedexes">
+        <Pokedexes />
+        </Tab>
+        <Tab eventKey="Version" title="Version">
+        <Version />
+        </Tab>
+        <Tab eventKey="Version-Group" title="Version-Group" >
+        <VersionGroup />
+        </Tab>
+      </Tabs>
+
+    
+    </>
+  );
 }
