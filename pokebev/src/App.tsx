8746b6f242c278169cc6189ed4b34f54c01239c6
laura.deoliveira <laura.deoliveira@ambevtech.com.br>
2021-10-28 12:12:42 -0300

    WE ARE THE CHAMPIONS FUNFOU

--- pokebev/src/App.tsx
+++ pokebev/src/App.tsx
@@ -1,48 +1,17 @@
-import  {useEffect, useState} from 'react'
-import Berry from  './components/Berry/Berry';
-import { BerryData } from './components/Models/interfaces';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import Contests from './components/Contest/Contest';
 import './App.css';
+import Berries from './components/Berry/Berries/Berries'
 
 function AppPokemon() {
 
 
-  //berries
-  const [berries, setBerries] = useState<BerryData[]>([]);
-
-
-  useEffect(() => { //faz a chamada, mas nao tem resposta
-    fetch('https://pokeapi.co/api/v2/berry?limit=8000')
-    .then((response) => response.json()) //espera a resposta
-    .then((data) => setBerries(data.results)); //espera o json ficar pronto
-  }, []);
-
   
-
-  // //contests
-  // const [contests, setContests] = useState<PokemonData[]>([]);
-  // useEffect(() => { //faz a chamada, mas nao tem resposta
-  //   fetch('https://pokeapi.co/api/v2/contests?limit=3')
-  //   .then((response) => response.json()) //espera a resposta
-  //   .then((data) => setContests(data.results)); //espera o json ficar pronto
-  // }, []);
 
   return (
     
     <div>
-      <h1>Berry</h1>
-      <div className="Berries-List">
-        {
-          berries.length > 0 && berries.map((berry) => <Berry url={berry.url} name={berry.name}  ></Berry>) //se a primeira for falsa, para.
-        }
-      </div>
-
-      {/* <div className="Contests-List">
-        {
-          contests.length > 0 && pokemons.map((pokemon) => <Contests url={pokemon.url} name={pokemon.name} ></Contests>) //se a primeira for falsa, para.
-        }
-      </div> */}
+      <Berries />
     </div>
     
   )
