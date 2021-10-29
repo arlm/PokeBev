b4f8243fa09d4c742cac24ab64ac6daf20847a58
laura.deoliveira <laura.deoliveira@ambevtech.com.br>
2021-10-29 17:28:00 -0300

    Mudando descrição home grupo 1

--- pokebev/src/components/GroupDetails/Bulbasaur/Bulbasaur.tsx
+++ pokebev/src/components/GroupDetails/Bulbasaur/Bulbasaur.tsx
@@ -4,31 +4,31 @@ function Bulbasaur() {
   return (
     <>
       <p>
-        <h1>Responsáveis pelo desenvolvimento do componente <a className={styles.link} href="/Games" >Games</a></h1>
+        <h1>Responsáveis pelo desenvolvimento do componente <a className={styles.link} href="/Berries" >Berries</a></h1>
         
       </p>
       <div className="d-flex flex-row justify-content-center">
         <div className={styles.ajuste}>
           <img
             className={styles.figura}
-            src="https://media0.giphy.com/media/loLOM9vO0g8TwsVkFu/giphy.gif?cid=790b76119b86175510422ae390ebbb98b1a91b0653b0bc4e&rid=giphy.gif&ct=g"
+            src="https://media3.giphy.com/media/I2nZMy0sI0ySA/giphy.gif?cid=790b761124fd093d203c583ec2b32049954e0f065deaba1a&rid=giphy.gif&ct=g"
           ></img>
         </div>
         <div>
           <p>
-            <h1>Gabe Berté</h1>
+            <h1>Clara Vasques</h1>
           </p>
           <p>
-            <h1>Giu Zambot</h1>
+            <h1>Laila Cardoso</h1>
           </p>
           <p>
-            <h1>Jislane Santana</h1>
+            <h1>Laurão Ribeiro</h1>
           </p>
           <p>
-            <h1>Laurinha Ribeiro</h1>
+            <h1>Paloma Calado</h1>
           </p>
           <p>
-            <h1>Mayhhara Morais</h1>
+            <h1>Úrsula Ariel</h1>
           </p>
         </div>
       </div>
